ffd20175d1a110a603d30e112cd25803b91f1c39
Acceldracyan <acceldracyan1@gmail.com>
2025-12-27 13:11:49 +0800

    HomePage V0.1

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
-
+    showUpcomingPetEvent();
     messageCycle();
-
 });
 
 
@@ -29,4 +28,77 @@ function messageCycle() {
         `;
     }, 3000);
 }
+
+function showUpcomingPetEvent() {
+    const events = [
+        new PetEvent("Happy Pet Event", "Kuala Lumpur", "Lorem impsump", "", "assets/img/Poster1.png"),
+        new PetEvent("Pet Even Ultimate", "Johor Bahru", "Lorem impsump", "", "assets/img/Poster2.png"),
+        new PetEvent("Fur-Day", "Kuala Nerus", "Lorem impsump", "", "assets/img/Poster3.png"),
+    ]
+
+
+    let carousel = document.getElementById("upcoming-event");
+    let indicator = document.getElementById("event-carousel-indicator")
+
+
+    let text = "";
+    let indicatorText = "";
+    let index = 0;
+    let isActive = "active";
+
+    events.forEach(event => {
+
+        //Slides
+        text += `<div class="carousel-item ${isActive}">
+                            <div class="row align-items-center slide-event-container">
+
+                                <!-- Image Column -->
+                                <div class="col-lg-3 col-12">
+                                    <img src="${event.poster}" alt="Poster" class="img-fluid">
+                                </div>
+
+                                <!-- Info Column -->
+                                <div class="col-lg-5 col-12">
+                                    <h2 class="text-center text-lg-start">${event.eventName}</h2>
+                                    <p class="text-center text-lg-start">Location: ${event.location}</p>
+                                    <p class="text-justify">${event.description}</p>
+                                </div>
+
+                                <!-- Social Column -->
+                                <div class="col-lg-3 col-12 social-container">
+                                    <h3 class="text-center">Follow Us</h3>
+                                    <div class="p-3 text-center">
+                                        <i class="fa-brands fa-facebook fa-xl"></i>
+                                        <i class="fa-brands fa-youtube fa-xl"></i>
+                                        <i class="fa-brands fa-instagram fa-xl"></i>
+                                        <i class="fa-brands fa-tiktok fa-xl"></i>
+                                        <i class="fa-brands fa-twitter fa-xl"></i>
+                                        <i class="fa-regular fa-map fa-xl"></i>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>`;
+
+        //Indicators
+        indicatorText += `<button type="button" data-bs-target="#eventCarousel" data-bs-slide-to=${index} class="${isActive}"></button>`;
+
+        if (isActive === "active") { isActive = ""; }
+        index ++;
+    });
+
+    carousel.innerHTML = text;
+    indicator.innerHTML = indicatorText;
+}
+
+
+function PetEvent(eventName, location, description, social, poster) {
+    this.eventName = eventName;
+    this.location = location;
+    this.description = description;
+    this.social = social;
+    this.poster = poster;
+}
+
+
 
