a975b523802187af26c915cb81f4f2b234bf98dd
Acceldracyan <acceldracyan1@gmail.com>
2025-12-28 09:41:22 +0800

    Homepage V1.1

--- js/javascript.js
+++ js/javascript.js
@@ -31,9 +31,9 @@ function messageCycle() {
 
 function showUpcomingPetEvent() {
     const events = [
-        new PetEvent("Happy Pet Event", "Kuala Lumpur", "Lorem impsump", "", "assets/img/Poster1.png"),
-        new PetEvent("Pet Even Ultimate", "Johor Bahru", "Lorem impsump", "", "assets/img/Poster2.png"),
-        new PetEvent("Fur-Day", "Kuala Nerus", "Lorem impsump", "", "assets/img/Poster3.png"),
+        new PetEvent("Happy Pet Event", "Kuala Lumpur", "23 November 2026", "1:00 PM - 5:00 PM", "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo dolorum cumque atque, ad quos voluptate nulla reiciendis nihil tempora praesentium debitis sit eaque iusto obcaecati odio sunt nam, modi impedit? impsump", "", "assets/img/Poster1.png"),
+        new PetEvent("Pet Even Ultimate", "Johor Bahru", "6 January 2026", "8:30 AM - 12:00 PM", "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo dolorum cumque atque, ad quos voluptate nulla reiciendis nihil tempora praesentium debitis sit eaque iusto obcaecati odio sunt nam, modi impedit?", "", "assets/img/Poster2.png"),
+        new PetEvent("Fur-Day", "Kuala Nerus","10 February 2026", "2:00 PM - 9:00 PM", "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nemo dolorum cumque atque, ad quos voluptate nulla reiciendis nihil tempora praesentium debitis sit eaque iusto obcaecati odio sunt nam, modi impedit?", "", "assets/img/Poster3.png"),
     ]
 
 
@@ -59,9 +59,17 @@ function showUpcomingPetEvent() {
 
                                 <!-- Info Column -->
                                 <div class="col-lg-5 col-12">
+
+
                                     <h2 class="text-center text-lg-start">${event.eventName}</h2>
-                                    <p class="text-center text-lg-start">Location: ${event.location}</p>
-                                    <p class="text-justify">${event.description}</p>
+                                    <div class = "text-event-info">
+                                        <div class="datetime my-3">
+                                            <p class="text-center text-lg-start">Date: ${event.date}</p>
+                                            <p class="text-center text-lg-start">Time: ${event.time}</p>
+                                        </div>
+                                        <p class="text-center text-lg-start">Location: ${event.location}</p>
+                                        <p class="text-justify">${event.description}</p>
+                                    </div>
                                 </div>
 
                                 <!-- Social Column -->
@@ -84,7 +92,7 @@ function showUpcomingPetEvent() {
         indicatorText += `<button type="button" data-bs-target="#eventCarousel" data-bs-slide-to=${index} class="${isActive}"></button>`;
 
         if (isActive === "active") { isActive = ""; }
-        index ++;
+        index++;
     });
 
     carousel.innerHTML = text;
@@ -92,9 +100,11 @@ function showUpcomingPetEvent() {
 }
 
 
-function PetEvent(eventName, location, description, social, poster) {
+function PetEvent(eventName, location, date, time, description, social, poster) {
     this.eventName = eventName;
     this.location = location;
+    this.date = date;
+    this.time = time;
     this.description = description;
     this.social = social;
     this.poster = poster;
